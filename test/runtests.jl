dbb98fcfe3bbba5e2874b8e39b7a30a278d09cf2
Adrian Egger <ae.ethz@gmail.com>
2022-02-28 20:42:27 +0100

    sample tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,21 @@
-using AnalyticalSolutions
+#using AnalyticalSolutions
 using Test
 
-@testset "AnalyticalSolutions.jl" begin
-    # Write your tests here.
-end
+foo(x) = length(x)^2
+
+@testset verbose = true "Foo Tests" begin
+    @testset "Animals" begin
+        @test foo("cat") == 9
+        @test foo("dog") == foo("cat")
+    end
+    @testset "Arrays $i" for i in 1:3
+        @test foo(zeros(i)) == i^2
+        @test foo(fill(1.0, i)) == i^2
+    end
+end;
 
 #=
 - Fall thru for abstract types
 
 =#
+
