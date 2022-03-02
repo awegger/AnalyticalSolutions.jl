f34be28263dc5defe0a18b4f15bed9e091f7e682
Documenter.jl <documenter@juliadocs.github.io>
2022-03-02 22:32:10 +0000

    build based on 8ff4804

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AnalyticalSolutions","category":"page"},{"location":"#AnalyticalSolutions","page":"Home","title":"AnalyticalSolutions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AnalyticalSolutions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AnalyticalSolutions]","category":"page"}]
+}
